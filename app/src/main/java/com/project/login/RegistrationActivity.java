37f9007ef6ee25707ccee3bce7665d403b16fed5
R4n070 <160589809+R4n070@users.noreply.github.com>
2024-09-18 14:39:54 -0200

    Initial commit

---
+++ app/src/main/java/com/project/login/RegistrationActivity.java
@@ -0,0 +1,34 @@
+package com.project.login;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrationActivity extends AppCompatActivity {
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+
+        EditText usernameEditText = findViewById(R.id.username);
+        EditText emailEditText = findViewById(R.id.email);
+        EditText passwordEditText = findViewById(R.id.password);
+       Button registerButton = findViewById(R.id.btnregister);
+
+       registerButton.setOnClickListener(v ->{
+
+           Intent intent = new Intent( RegistrationActivity.this, MainActivity.class);
+
+
+
+       });
+
+    }
+}
+
